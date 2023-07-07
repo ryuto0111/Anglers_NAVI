6759d38229ed3946982ca9e02a5964a4179cb0ec
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-07-07 04:59:20 +0000

    [Add]Association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  has_many :users
+  belongs_to :users
   has_many :post_tags,                dependent: :destroy
   has_many :tags, through: :post_tag, dependent: :destroy
+  has_many :favorites,                dependent: :destroy
+  has_many :post_comments,            dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :entries,  dependent: :destroy
-  has_many :messages, dependent: :destroy
+  has_many :entries,       dependent: :destroy
+  has_many :messages,      dependent: :destroy
+  has_many :posts,         dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites,     dependent: :destroy
+  has_many :relationships,            class_name: "Relationships", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_re_relationships, class_name: "Relationships", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships,            source: :followed
+  has_many :followers,  through: :reverse_of_relationships, source: :follower
 end
