2ac8b76759c43b11bb4df1246bf4c66c591bcf46
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-07-08 15:22:05 +0000

    [Add]gem-pry/admin-user-manage

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,32 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @user = User.page(params[:page]).per(10)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報の更新に成功しました。"
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :email, :is_deleted)
+  end
+
 end
