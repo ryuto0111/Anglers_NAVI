baf37288c9a29a0e96f0ceb5601b410b1bed7b66
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-07-11 16:51:26 +0000

    [Save]

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -5,11 +5,19 @@ class Public::PostCommentsController < ApplicationController
     @comment = current_user.post_comments.new(post_comment_params)
     @comment.post_id = @post.id
     @comment.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = PostComment.find(params[:id])
+    @comment.destroy
+    redirect_to request.referer
   end
 
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:comment_content, :post_id)
+    params.require(:post_comment).permit(:comment_content)
   end
 end
