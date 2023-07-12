808013657f5fa8a36bcd38efa5bf573e34d888f2
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-07-12 17:06:56 +0000

    [Save]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,14 +2,14 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-     @current_entry = Entry.where(user_id: current_user.id)
+    @current_entry = Entry.where(user_id: current_user.id)
     @another_entry = Entry.where(user_id: @user.id)
     unless @user.id == current_user.id
       @current_entry.each do |current|
         @another_entry.each do |another|
           if current.room_id == another.room_id
-            @is_room = true
-            @room_id = current.room_id
+            @isRoom = true
+            @roomId = current.room_id
           end
         end
       end
