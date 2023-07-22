d5abc39f1b8483f3350b571b8476faa7d8e62a94
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-07-22 04:33:02 +0000

    [Add]validate

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,4 +1,7 @@
 class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :comment_content, presence:true, length: {maximum: 200 }
+
 end
--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,4 +1,5 @@
 class PostTag < ApplicationRecord
   belongs_to :post
   belongs_to :tag
+  validates :name, presence: true, length:{ minimum: 2, maximum: 10 }, uniqueness: true
 end
