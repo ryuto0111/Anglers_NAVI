48cbd08528289e3cf3206f19d42df0027c705571
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-07-27 17:12:22 +0000

    [save]

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    post "users/guest_sign_in", to: "public/sessions#guest_sign_in"
+    post "guest_sign_in", to: "public/sessions#guest_sign_in"
   end
 
   # 管理者用
